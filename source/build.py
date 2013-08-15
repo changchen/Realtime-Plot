d3188762f08085aa37bcac1715c65234ba42a174
Chen Chang <chenchangs@gmail.com>
2013-08-15 16:52:57 +0800

    back to work

--- source/build.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-command = "pyuic4 -o ui_realtimeplot.py realtimeplot.ui"
-print command
-os.system(command)
--- source/main.py
+++ source/main.py
@@ -3,47 +3,98 @@ import sys
 #from window import Window
 #from PyQt4.QtCore import Qt, SIGNAL
 from PyQt4.QtGui import *
+from PyQt4 import QtCore
 from ui_realtimeplot import Ui_MainWindow
-import serial
+## import serial
 
-class SerialPort:
-    """Serial port init, config, data receive and data sending."""
-    def __init__(self):
-        try:
-            self.ser = ser = serial.Serial(
-                port='com57',
-                baudrate=115200,
-                bytesize=serial.EIGHTBITS,
-                parity=serial.PARITY_NONE,
-                stopbits=serial.STOPBITS_ONE,
-                timeout=0.1,
-                xonxoff=0,
-                rtscts=0,
-                interCharTimeout=None
-            )
 
-        except serial.serialutil.SerialException:
-            #no serial connection
-            self.ser = None
-        else:
-            #Thread(target=receiving, args=(self.ser,)).start()
-            pass
-            
-    def SerDataReceive(self):
-        pass
+## class SerialPort(serial.Serial):
+##     """Serial port init, config, data receive and data sending."""
+
+##     def __init__(self):
+##         try:
+##             self.ser = serial.Serial(
+##                 port='com1',
+##                 baudrate=115200,
+##                 bytesize=serial.EIGHTBITS,
+##                 parity=serial.PARITY_NONE,
+##                 stopbits=serial.STOPBITS_ONE,
+##                 timeout=0.1,
+##                 xonxoff=0,
+##                 rtscts=0,
+##                 interCharTimeout=None
+##             )
+##         except serial.serialutil.SerialException:
+##             #no serial connection
+##             self.ser = None
+##         else:
+##             #Thread(target=receiving, args=(self.ser,)).start()
+##             pass
+
+##     def Connect(self):
+##         pass
+
+##     def Disconnect(self):
+##         pass
+
+
+## class DataReader(QtCore.QThread):
+##     "loop and copy serial->console"
+##     def __init__(self, port_src=None):
+##         #QtCore.QThread.__init__(self)
+##         super(DataReader, self).__init__()
+##         self.port_src = port_src
+
+##     def run(self):
+##         while 1:
+##             ser_data_queue = self.port_src.read(1)
+##             window.plainTextEdit.appendPlainText('s')
+
+
+## class DataWriter(QtCore.QThread):
+##     ""
+##     def __init__(self, port_tar=None):
+##         #QtCore.QThread.__init__(self)
+##         super(DataReader, self).__init__()
+##         self.port_tar = port_tar
+
+##     def run(self):
+##         for i in range(50):
+##             print 'W%r' % (i)
+
+
+## def data_dispatch():
+##     "send the data to terminal window or plot panel"
+##     pass
+
+
+## def plot():
+##     "plot the data"
+##     pass
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 
-    def __init__(self, parent = None):
-        
+    def __init__(self, parent=None):
+
         QWidget.__init__(self, parent)
         self.setupUi(self)
+        self.actionOpen_port.triggered.connect(self.openPort)
+        self.actionClose_port.triggered.connect(self.closePort)
+        
+    def openPort(self):
+        print 'Open port'
+        
+    def closePort(self):
+        print 'Close port'
 
 
-if __name__ == "__main__":
+def main():
 
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
