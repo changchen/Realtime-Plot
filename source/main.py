f32b133abdb2b74e8658c8fbedaf3d3f92710abb
Chen Chang <chenchangs@gmail.com>
2013-11-06 13:31:51 +0800

    add class ReadProt & WritePort

--- source/main.py
+++ source/main.py
@@ -1,7 +1,7 @@
 import sys
 import serial
 from PyQt4.QtGui import QApplication, QMainWindow
-from PyQt4 import QtCore
+from PyQt4.QtCore import QThread
 from ui_mainWindow import Ui_MainWindow
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -11,10 +11,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.ui.setupUi(self)
 
     def openPort(self):
-        print 'Open port'
+        " Open serial port "
 
     def closePort(self):
-        print 'Close port'
+        " Close serial port "
+
+class ReadPort(QThread):
+    def start(self):
+        " Start receiving data from port "
+
+    def run(self):
+        " Reading data from port "
+
+    def terminate(self):
+        " Terminate data receiving "
+
+class WritePort(QThread):
+    def start(self):
+        " Start sending data to port "
+
+    def run(self):
+        " Sending data to port "
+
+    def terminate(self):
+        " Terminate sending data "
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
