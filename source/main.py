c4f9bfc182d54665db2c766536af742335608c8b
Chen Chang <chenchangs@gmail.com>
2013-11-11 16:30:14 +0800

    make serial connection funtion work

--- source/main.py
+++ source/main.py
@@ -1,7 +1,7 @@
 import sys
 import serial
 import serial.tools.list_ports
-from PyQt4.QtGui import QApplication, QMainWindow
+from PyQt4.QtGui import QApplication, QMainWindow, QTextCursor
 from PyQt4.QtCore import QThread, QObject # TODO: SIGNAL
 from ui_mainWindow import Ui_MainWindow
 
@@ -12,13 +12,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.setupUi()
+        self.ser = None
+        self.receiveData = ReadPort()
+        self.sendData = WritePort()
 
     def setupUi(self):
         self.ui.setupUi(self)
         self.ui.comboBox_Bandrate.addItems(baudRates)
         self.refreshComboBoxPortName()
-        # TODO
-        #QObject.connect(self.ui.comboBox_PortName, SIGNAL("activated()"), self.refreshComboBoxPortName)
+        # TODO QObject.connect(self.ui.comboBox_PortName, SIGNAL("activated()"), self.refreshComboBoxPortName)
+        self.ui.pushButton_OpenPort.clicked.connect(self.openPort)
+        self.ui.pushButton_ClosePort.clicked.connect(self.closePort)
 
     def refreshComboBoxPortName(self):
         self.ui.comboBox_PortName.clear()
@@ -30,21 +34,78 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             portNames.append(port[0])
         return portNames
 
+    def getPortConfig(self):
+        " Get port setting from ui "
+        print self.ui.comboBox_PortName.currentText()
+        return self.ui.comboBox_PortName.currentText()
+
+    def getBandRateConfig(self):
+        " Get baudrate setting from ui "
+        print self.ui.comboBox_Bandrate.currentText()
+        return self.ui.comboBox_Bandrate.currentText()
+
+    def printInfo(self, text):
+        " print information to ui "
+        self.ui.plainTextEdit_txtDisplay.appendPlainText(text)
+        self.ui.plainTextEdit_txtDisplay.moveCursor(QTextCursor.End)
+
+    def printError(self, text):
+        " print error to ui "
+        self.ui.plainTextEdit_txtDisplay.appendPlainText(text)
+        self.ui.plainTextEdit_txtDisplay.moveCursor(QTextCursor.End)
+
     def openPort(self):
         " Open serial port "
+        self.closePort()
+        try:
+            print("Connecting to %s with %s baud rate." % (self.getPortConfig(), self.getBandRateConfig()))
+            self.printInfo("Connecting to %s with %s baud rate." % (self.getPortConfig(), self.getBandRateConfig()))
+            self.ser = serial.Serial(str(self.getPortConfig()), int(self.getBandRateConfig()))
+            self.receiveData.start(self.ser)
+            self.printInfo("->>- Connected successfully.")
+        except:
+            self.ser = None
+            self.printError("Failed to connect!")
 
     def closePort(self):
         " Close serial port "
+        if self.receiveData.isRunning():
+            self.receiveData.terminate()
+        if self.sendData.isRunning():
+            self.sendData.terminate()
+        if self.ser == None: return
+        try:
+            if self.ser.isOpen:
+                self.ser.close()
+                self.printInfo("-<>- Disconnected successfully.")
+        except:
+            self.printError("Failed to disconnect!")
+        self.ser = None
+
 
 class ReadPort(QThread):
-    def start(self):
+    def start(self, ser, priority = QThread.InheritPriority):
         " Start receiving data from port "
+        self.ser = ser
+        QThread.start(self, priority)
 
     def run(self):
         " Reading data from port "
+        while True:
+            try:
+                data = self.ser.read(1)
+                n = self.ser.inWaiting()
+                if n:
+                    data = data + self.ser.read(n)
+                self.emit(SIGNAL("newData(QString)"), data)
+            except:
+                errMsg = "Reader thread is terminated unexpectedly."
+                self.emit(SIGNAL("error(QString)"), errMsg)
+                break
 
     def terminate(self):
         " Terminate data receiving "
+
 
 class WritePort(QThread):
     def start(self):
